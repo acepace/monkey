ea22aa97fbe28b9ac93615cc3ef5b2c1ad6a8774
Ace Pace <acepace@gmail.com>
2020-05-11 18:17:37 +0300

    Fingerprint http and https

--- monkey/infection_monkey/network/elasticfinger.py
+++ monkey/infection_monkey/network/elasticfinger.py
@@ -30,19 +30,20 @@ class ElasticFinger(HostFinger):
         :param host:
         :return: Success/failure, data is saved in the host struct
         """
-        try:
-            url = 'http://%s:%s/' % (host.ip_addr, ES_PORT)
-            with closing(requests.get(url, timeout=ES_HTTP_TIMEOUT)) as req:
-                data = json.loads(req.text)
-                self.init_service(host.services, ES_SERVICE, ES_PORT)
-                host.services[ES_SERVICE]['cluster_name'] = data['cluster_name']
-                host.services[ES_SERVICE]['name'] = data['name']
-                host.services[ES_SERVICE]['version'] = data['version']['number']
-                return True
-        except Timeout:
-            LOG.debug("Got timeout while trying to read header information")
-        except ConnectionError:  # Someone doesn't like us
-            LOG.debug("Unknown connection error")
-        except KeyError:
-            LOG.debug("Failed parsing the ElasticSearch JSOn response")
+        urls = ['{}://{}:{}'.format(protocol, host.ip_addr, ES_PORT) for protocol in ['http', 'https']]
+        for url in urls:
+            try:
+                with closing(requests.get(url, timeout=ES_HTTP_TIMEOUT, verify=False)) as req:
+                    data = json.loads(req.text)
+                    self.init_service(host.services, ES_SERVICE, ES_PORT)
+                    host.services[ES_SERVICE]['cluster_name'] = data['cluster_name']
+                    host.services[ES_SERVICE]['name'] = data['name']
+                    host.services[ES_SERVICE]['version'] = data['version']['number']
+                    return True
+            except Timeout:
+                pass
+            except ConnectionError:  # Someone doesn't like us
+                pass
+            except KeyError:
+                LOG.debug("Failed parsing the ElasticSearch JSON response")
         return False
